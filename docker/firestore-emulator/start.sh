8d77be05cca7c1af1af1d4037657a665cbb3ecb0
Robert Laszczak <roblaszczak@gmail.com>
2022-05-24 16:59:01 +0200

    Added support for M1 CPU

---
+++ docker/firestore-emulator/start.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+set -xe
+
+firebase emulators:start
--- sql/schema.sql
+++ sql/schema.sql
@@ -1,6 +1,6 @@
 CREATE TABLE `hours`
 (
-    hour         TIMESTAMP                                                 NOT NULL,
+    hour         TIMESTAMP                                                 NOT NULL DEFAULT 0,
     availability ENUM ('available', 'not_available', 'training_scheduled') NOT NULL,
     PRIMARY KEY (hour)
 );
